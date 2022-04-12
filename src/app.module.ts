ea2a78ac28953578467b96a21d18e037dcdba41d
Vlad Nicula <vladnicula@gmail.com>
2022-04-12 16:05:54 +0300

    Baisc sqlite db and users entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import sqlite3 from 'sqlite3'
 
 import configuration from './config/configuration';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.modules';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    load: [configuration],
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      driver: sqlite3,
+      database: "invoice-app-db",
+      synchronize: true,
+      entities: [
+        User
+      ]
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, HttpCode, Param, Query, Res } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { query, Response } from 'express';
+
+@Controller()
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get('/users')
+    @HttpCode(200)
+    async getUsers(@Res() res: Response) {
+        const users = await this.usersService.findAll();
+        return res.json({users, total: users.length});
+    }
+} 
