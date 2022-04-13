fe2cff9095a5bb87ce94a3e81c001f7080e05560
Vlad Nicula <vladnicula@gmail.com>
2022-04-13 11:21:48 +0300

    Cors enabled

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   // swagger config
   const config = new DocumentBuilder()
@@ -14,7 +14,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
 
   await app.listen(process.env.PORT);
 }
